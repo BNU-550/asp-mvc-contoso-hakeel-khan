4be765b075f84601b36cd928b60f28f6c840dddd
hakee <hakee@LAPTOP-84U9VAUD>
2021-11-21 20:50:59 +0000

    Course enrollment

--- ASP_MVC_Contoso/Data/ApplicationDbContext.cs
+++ ASP_MVC_Contoso/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ASP_MVC_Contoso.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,8 @@ namespace ASP_MVC_Contoso.Data
             : base(options)
         {
         }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
     }
 }
