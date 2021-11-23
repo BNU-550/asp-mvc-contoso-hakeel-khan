2d715bc44b9dd84fcd0370b15c0a608ed3c3d47d
hakee <hakee@LAPTOP-84U9VAUD>
2021-11-23 12:39:43 +0000

    Details update

--- ASP_MVC_Contoso/Controllers/StudentsController.cs
+++ ASP_MVC_Contoso/Controllers/StudentsController.cs
@@ -73,7 +73,11 @@ namespace ASP_MVC_Contoso.Controllers
                 return NotFound();
             }
 
-            var student = await _context.Students.FindAsync(id);
+            var student = await _context.Students
+                .Include(s => s.Enrollments)
+                    .ThenInclude(e => e.Course)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.StudentID == id);
             if (student == null)
             {
                 return NotFound();
