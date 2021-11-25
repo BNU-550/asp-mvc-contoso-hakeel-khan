d11448efb3d5c25155a0319d87f2b35b815b8d91
hakee <hakee@LAPTOP-84U9VAUD>
2021-11-25 11:52:02 +0000

    Search update

--- ASP_MVC_Contoso/Controllers/StudentsController.cs
+++ ASP_MVC_Contoso/Controllers/StudentsController.cs
@@ -20,12 +20,32 @@ namespace ASP_MVC_Contoso.Controllers
         }
 
         // GET: Students
-        public async Task<IActionResult> Index(string sortOrder)
+        public async Task<IActionResult> Index(string sortOrder, string searchString,
+            string currentFilter, Nullable<int> pageNumber)
         {
+            ViewData["CurrentSort"] = sortOrder;
             ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewData["CurrentFilter"] = searchString;
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
             var students = from s in _context.Students
                            select s;
+
+            if(!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.Contains(searchString)
+                                    || s.FirstName.Contains(searchString));
+            }
+
             switch (sortOrder)
             {
                 case "name_desc":
@@ -41,7 +61,8 @@ namespace ASP_MVC_Contoso.Controllers
                     students = students.OrderBy(s => s.LastName);
                     break;
             }
-            return View(await students.AsNoTracking().ToListAsync());
+            int pageSize = 4;
+            return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
         // GET: Students/Details/5
